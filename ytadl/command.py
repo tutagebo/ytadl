f6cfd43e79720b183f608a1093fbd701236dd4c8
tutagebo <koudai.nattoufu@gmail.com>
2025-03-27 00:51:03 +0900

    able to download video

--- ytadl/command.py
+++ ytadl/command.py
@@ -1,12 +1,16 @@
 import click
-from .downloader import download_audio
+from .downloader import download_audio, download_video
 
 @click.command()
 @click.argument("url")
 @click.argument("output_dir", type=click.Path(exists=True))
 @click.option("--ffmpeg", "ffmpeg_location", type=click.Path(), help="select ffmpeg's path")
-def main(url, output_dir, ffmpeg_location):
-    download_audio(url, output_dir, ffmpeg_location)
+@click.option('-v','--video', is_flag=True, help='youtubeの動画をダウンロードするモード')
+def main(url, output_dir, ffmpeg_location, video):
+    if video:
+        download_video(url, output_dir, ffmpeg_location)
+    else:
+        download_audio(url, output_dir, ffmpeg_location)
 
 if __name__ == "__main__":
     main()
--- ytadl/downloader.py
+++ ytadl/downloader.py
@@ -17,3 +17,14 @@ def download_audio(url, output_dir, ffmpeg_location=None):
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+def download_video(url, output_dir, ffmpeg_location=None):
+    ydl_opts = {
+        "format": "best",
+        "outtmpl": os.path.join(output_dir, "%(title)s.%(ext)s")
+    }
+    if ffmpeg_location:
+        ydl_opts["ffmpeg_location"] = ffmpeg_location
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
